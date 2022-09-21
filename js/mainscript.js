5aef82fad0f7db45a979939e4fd0548cdeec1c2a
glanshima <glanshima@gmail.com>
2022-09-21 23:43:24 +0100

    layout slides fixed

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,13 +1,13 @@
 /* const anime = require('animejs'); */
 
 
-const contactForm = document.querySelector('.contact');
+const contactForm = document.querySelector('.contact-form-section');
 const contactUs = document.querySelector('.contact-form');
 const staffLogin = document.querySelector('.staff-login-form');
 const contactBtn = document.querySelector('.contact-select');
 const staffLoginBtn = document.querySelector('.login-select');
 const ourTeamText = document.querySelector('.our-team-hd');
-const closeForm = document.querySelector('.close');
+const closeForm = document.querySelector('.form-close');
 const teamFrame = document.querySelector('.team');
 const siteTitle = document.querySelector('.site-title');
 const serviceContainer = document.querySelector('.service-container');
@@ -15,6 +15,7 @@ const flashHeroBox = document.querySelector('.flash-hero');
 const flashImgWide = document.querySelector('#flash-img-wide');
 const flashImgNarrow = document.querySelector('#flash-img-narrow');
 const serviceDescription = document.querySelector('.service-description');
+const productSection = document.querySelector('.our-products');
 /* anime({
     targets: 'div.center-box',
     left: [{
@@ -42,7 +43,7 @@ function clickEvents(type, selector, callback) {
 
 contactForm.addEventListener('click', (e) => {
     if (e.target.matches('.contact-select')) {
-        contactForm.style.cssText = "height: 27rem";
+        contactForm.style.cssText = "height: 26rem; padding-bottom: 1rem";
         contactUs.style.cssText = "right: 0";
         staffLogin.style.cssText = "right: -100";
         closeForm.style.cssText = "visibility: visible";
@@ -51,8 +52,10 @@ contactForm.addEventListener('click', (e) => {
         flashHeroBox.classList.remove('login-clicked')
         flashHeroBox.style.cssText = "visibility: visible";
         serviceDescription.style.cssText = "display: none";
+        productSection.style.cssText = "display: flex";
         flashImgNarrow.style.cssText = "translate: 0";
         flashImgWide.style.cssText = "translate:-100%";
+
 
 
     } else if (e.target.matches('.login-select')) {
@@ -67,7 +70,9 @@ contactForm.addEventListener('click', (e) => {
         flashImgWide.style.cssText = "translate: 0";
         flashImgNarrow.style.cssText = "translate: -100%";
         serviceDescription.style.cssText = "display: none";
-    } else if (e.target.matches('.close')) {
+        productSection.style.cssText = "display: flex";
+
+    } else if (e.target.matches('.form-close')) {
         contactForm.style.cssText = "height:3.5rem";
         staffLogin.style.cssText = "right: -100";
         contactUs.style.cssText = "right: -100";
@@ -77,6 +82,8 @@ contactForm.addEventListener('click', (e) => {
         flashHeroBox.classList.remove('login-clicked')
         serviceDescription.style.cssText = "display: flex";
         flashHeroBox.style.cssText = "visibility: hidden";
+        productSection.style.cssText = "display: none";
+
 
 
     }
@@ -126,32 +133,9 @@ setInterval(() => {
     siteTitle.classList.toggle("glow")
 }, 5000)
 
-/* Glide  library */
-new Glide('.glide').mount()
-
-var glide = new Glide('#intro', {
-    type: 'carousel',
-    perView: 4,
-    focusAt: 'center',
-    breakpoints: {
-        800: {
-            perView: 2
-        },
-        480: {
-            perView: 1
-        }
-    }
+const slidingImg = document.querySelectorAll('.carousel-item.img-mine');
+slidingImg.forEach(imgSlide => {
+    setTimeout(() => {
+        imgSlide.classList.toggle("scale")
+    }, 2500);
 })
-
-glide.mount()
-
-
-const animateShapeOut = () => {
-    anime({
-        targets: ('.gallery - img::after'),
-        duration: .5,
-        delay: 150,
-        easing: easing,
-
-    });
-}
