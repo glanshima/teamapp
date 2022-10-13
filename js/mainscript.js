18d44302a95bebb511a9201c234ad26795dcac20
glanshima <glanshima@gmail.com>
2022-10-13 21:59:44 +0100

    mobile fisrt view adjestments on layout before footer inclusion

--- js/mainscript.js
+++ js/mainscript.js
@@ -5,19 +5,21 @@ const contactUs = document.querySelector('.contact-form');
 const staffLogin = document.querySelector('.staff-login-form');
 const contactBtn = document.querySelector('.contact-select');
 const staffLoginBtn = document.querySelector('.login-select');
-const ourTeamText = document.querySelector('.our-team-hd');
+const ourTeamText = document.querySelector('.our-team-hd wide-screen');
 const closeForm = document.querySelector('.form-close');
 const teamFrame = document.querySelector('.team');
 const siteTitle = document.querySelector('.site-title');
-const serviceContainer = document.querySelector('.service-container');
+const serviceContainer = document.querySelector('service-container');
 const flashHeroBox = document.querySelector('.flash-hero');
 const flashImgWide = document.querySelector('#flash-img-wide');
 const flashImgNarrow = document.querySelector('#flash-img-narrow');
 const serviceDescription = document.querySelector('.service-description');
 const productSection = document.querySelector('.our-products');
 const slideCaption = document.querySelector('.slide-caption');
-const headerPos = header.getBoundingClientRect().top
-let windowHeight = window.innerHeight;
+const content = document.querySelector('.content');
+const formInput = document.querySelector('.input');
+const contentPos = content.getBoundingClientRect().top
+
 
 
 function clickEvents(type, selector, callback) {
@@ -27,32 +29,33 @@ function clickEvents(type, selector, callback) {
 }
 
 
-
+/* Sticky nav header  */
 function setStickyHeader(e) {
-    let sticky = header.offsetTop;
-
-    if (window.pageYOffset > sticky) {
+    let ScrollPos = window.scrollY;
+    if (ScrollPos > contentPos) {
 
         header.classList.add('sticky')
+
     } else {
         header.classList.remove('sticky')
     }
 }
-/* 
+
 window.addEventListener('scroll', e => {
 
     _.throttle(setStickyHeader(e), 100)
 
-}) */
+})
 
 
-
+/* Sticky nav header end  */
 
 /* Contact Section Form Toggle Events */
 
 contactForm.addEventListener('click', (e) => {
     if (e.target.matches('.contact-select')) {
         contactForm.style.cssText = "height: 28rem; padding-bottom: 1rem";
+
         contactUs.style.cssText = "right: 0";
         staffLogin.style.cssText = "right: -100";
         closeForm.style.cssText = "visibility: visible";
@@ -95,6 +98,10 @@ contactForm.addEventListener('click', (e) => {
     }
 })
 
+
+/* Contact Section Form Toggle Events  end*/
+
+
 /* Hover Effect on Meet our Team Text */
 teamFrame.addEventListener('mouseover', () => {
 
@@ -103,10 +110,14 @@ teamFrame.addEventListener('mouseover', () => {
 })
 teamFrame.addEventListener('mouseout', () => {
 
-        ourTeamText.style.opacity = '1';
+    ourTeamText.style.opacity = '1';
 
-    })
-    /* Flashing Site Title Text */
+})
+
+
+
+
+/* Flashing Site Title Text */
 
 setInterval(() => {
 
@@ -141,8 +152,6 @@ clickEvents('click', '.slide-nav__btn-icon', (e) => {
 })
 
 
-
-
 dots.forEach((dot, i) => {
 
     dot.addEventListener('click', (e) => {
@@ -153,10 +162,6 @@ dots.forEach((dot, i) => {
 
 
 })
-
-
-
-
 
 function startSlide(n) {
 
@@ -178,4 +183,107 @@ function nextSlide(n) {
 function prevSlide(n) {
     currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--;
     startSlide(currentSlide)
+}
+
+
+/* Team Member slide function */
+
+
+const teamMembers = document.querySelectorAll('.team-member');
+const counter = document.querySelector('.counter');
+const currentNumberOutput = document.querySelector('.current-number');
+let totalMembersOutput = document.querySelector('.total-members');
+let currentMember = 0;
+let currentMemberNumber = 1;
+let totalMembers = teamMembers.length;
+totalMembersOutput.textContent = totalMembers;
+currentNumberOutput.textContent = currentMemberNumber;
+window.addEventListener('DOMContentLoaded', e => {
+
+    viewNextMember(currentMember);
+})
+
+let memberIndex
+
+
+
+
+clickEvents('click', '.team_btn', e => {
+    if (e.target.matches('.prev-member')) {
+        prevMember(currentMember);
+
+        /* members counter logic */
+        currentMemberNumber <= 1 ? currentMemberNumber = teamMembers.length : currentMemberNumber--;
+
+        currentNumberOutput.textContent = currentMemberNumber;
+
+
+    } else if (e.target.matches('.next-member')) {
+        nextMember(currentMember);
+
+        /* members counter logic */
+        currentMemberNumber >= teamMembers.length ? currentMemberNumber = 1 : currentMemberNumber++;
+
+        currentNumberOutput.textContent = currentMemberNumber;
+
+    }
+})
+
+function viewNextMember(n) {
+    teamMembers.forEach(member => {
+        member.style.cssText = "display: none;";
+    })
+    teamMembers[n].style.cssText = "display: flex;";
+}
+/* setInterval(() => {
+    numCount()
+    nextMember();
+}, 2000); */
+
+function numCount() {
+    currentMemberNumber >= teamMembers.length ? currentMemberNumber = 1 : currentMemberNumber++;
+
+    currentNumberOutput.textContent = currentMemberNumber;
+
+}
+
+function nextMember() {
+    currentMember >= totalMembers - 1 ? currentMember = 0 : currentMember++;
+
+    viewNextMember(currentMember);
+
+
+
+}
+
+function prevMember(n) {
+    currentMember <= 0 ? currentMember = teamMembers.length - 1 : currentMember--;
+
+    viewNextMember(currentMember)
+}
+
+
+
+/* set layout for wide and small screen to show team members section */
+
+
+/* window.addEventListener("resize", setLayout()) */
+
+function setLayout() {
+    let teamSection = document.querySelector('.team')
+    if (window.innerWidth >= 650) {
+        teamMembers.forEach(member => {
+            teamSection.classList.toggle('wide-screen');
+
+            member.classList.remove('team-member');
+            member.classList.add('team-member-wide-screen');
+        })
+    } else if (window.innerWidth <= 649) {
+        teamMembers.forEach(member => {
+            teamSection.classList.toggle('wide-screen');
+
+            member.classList.remove('team-member-wide-screen')
+            member.classList.add('team-member')
+        })
+    }
 }
