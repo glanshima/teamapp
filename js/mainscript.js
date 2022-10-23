9add47bb76299f59da3a7191da15d26d35ab9cae
glanshima <glanshima@gmail.com>
2022-10-23 19:01:36 +0100

    fixed team responsiveness using resizeObserver

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,25 +1,18 @@
 /* const anime = require('animejs'); */
 const header = document.querySelector('.header');
 const footer = document.querySelector('.footer');
-
-const contactForm = document.querySelector('.contact-form.mobile-view-contact-form');
-const staffLogin = document.querySelector('.staff-login-form');
+const contactForm = document.querySelector('.contact-form');
 const contactBtn = document.querySelector('.contact-select');
-const staffLoginBtn = document.querySelector('.login-select');
 const ourTeamText = document.querySelector('.our-team-hd wide-screen');
-const closeForm = document.querySelector('.form-close');
+const closeFormBtn = document.querySelector('.form-close');
 const teamFrame = document.querySelector('.team');
 const siteTitle = document.querySelector('.site-title');
 const serviceContainer = document.querySelector('service-container');
-const flashHeroBox = document.querySelector('.flash-hero');
-const flashImgWide = document.querySelector('#flash-img-wide');
-const flashImgNarrow = document.querySelector('#flash-img-narrow');
-const serviceDescription = document.querySelector('.service-description');
-const productSection = document.querySelector('.our-products');
 const slideCaption = document.querySelector('.slide-caption');
 const content = document.querySelector('.content');
 const formInput = document.querySelector('.input');
-const contentPos = content.getBoundingClientRect().top
+const contentPos = content.getBoundingClientRect().top;
+let formOpenState = false;
 
 
 
@@ -52,16 +45,41 @@ window.addEventListener('scroll', e => {
 /* Sticky nav header end  */
 
 /* Contact Section Form Toggle Events */
-closeForm.addEventListener('click', () => {
-    contactForm.style.cssText = "translate: 200%; scale: .1;";
-})
+
 
 footer.addEventListener('click', (e) => {
     if (e.target.matches('.contact-us-icon')) {
-        contactForm.classList.add('formOpened')
-        contactForm.style.cssText = "translate: 0; scale: 1";
+        openForm()
+
+    } else if (e.target.matches('.form-close')) {
+        closeForm()
     }
 })
+content.addEventListener('click', (e) => {
+    /*  let isClose = e.target.closest(contactForm);
+     if (!isClose && contactForm.classList.contains('formOpened')) {
+         closeForm();
+     } */
+    closeForm()
+})
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        closeForm()
+    }
+})
+
+
+function openForm() {
+
+    contactForm.classList.add('formOpened')
+
+}
+
+
+function closeForm() {
+    contactForm.classList.remove('formOpened')
+
+}
 
 
 /* Contact Section Form Toggle Events  end*/
@@ -85,7 +103,6 @@ teamFrame.addEventListener('mouseout', () => {
 /* Flashing Site Title Text */
 
 setInterval(() => {
-
     siteTitle.classList.toggle("glow")
 }, 5000)
 
@@ -168,7 +185,7 @@ window.addEventListener('DOMContentLoaded', e => {
     viewNextMember(currentMember);
 })
 
-let memberIndex
+let memberIndex;
 
 
 
@@ -195,12 +212,69 @@ clickEvents('click', '.team_btn', e => {
 
 })
 
-function viewNextMember(n) {
+/* function viewNextMember(n) {
     teamMembers.forEach(member => {
         member.style.cssText = "display: none;";
     })
     teamMembers[n].style.cssText = "display: flex;";
+} */
+
+/* function viewNextMember(n) {
+    teamMembers.forEach(member => {
+        // Make window switch team views on different view ports 
+        window.addEventListener('load', () => {
+
+
+
+            if (window.innerWidth <= 649) {
+                member.style.cssText = "display: none;";
+                member.classList.add('mobile-viewport')
+            } else if (window.innerWidth >= 650) {
+                member.style.cssText = "display: flex;";
+                member.classList.remove('mobile-viewport')
+            }
+            teamMembers[n].style.cssText = "display: flex;";
+        })
+    })
+
+
+
+} */
+
+
+
+
+function viewNextMember(n) {
+    teamMembers.forEach(member => {
+        /* Make window switch team views on different view ports */
+        let body = document.querySelector('body')
+
+
+        const observer = new ResizeObserver(entries => {
+            body = entries[0]
+            const isMobile = body.contentRect.width <= 649;
+            if (isMobile === true) {
+                member.style.cssText = "display: none";
+                // member.classList.add('mobile-viewport')
+            } else {
+                member.style.cssText = "display: flex";
+                // member.classList.remove('mobile-viewport')
+            }
+            teamMembers[n].style.cssText = "display: flex;";
+
+        });
+        observer.observe(body)
+
+        /*  return teamMembers[n]; */
+
+
+
+    })
+
+
+
 }
+
 /* setInterval(() => {
     numCount()
     nextMember();
@@ -225,7 +299,7 @@ function nextMember() {
 function prevMember(n) {
     currentMember <= 0 ? currentMember = teamMembers.length - 1 : currentMember--;
 
-    viewNextMember(currentMember)
+    viewNextMember(currentMember);
 }
 
 
@@ -235,7 +309,7 @@ function prevMember(n) {
 
 /* window.addEventListener("resize", setLayout()) */
 
-function setLayout() {
+/* function setLayout() {
     let teamSection = document.querySelector('.team')
     if (window.innerWidth >= 650) {
         teamMembers.forEach(member => {
@@ -252,13 +326,13 @@ function setLayout() {
             member.classList.add('team-member')
         })
     }
-}
+} */
 
 
 /* testimonial slide functionality */
-let testimonialSlide = document.querySelectorAll('.testimonial-client.mobile-viewport');
+let testimonialSlide = document.querySelectorAll('.testimonial-client');
 let currentTestimony = 0;
-window.addEventListener('DOMContentLoaded', setTestimony(currentTestimony))
+window.addEventListener('DOMContentLoaded', setTestimony(currentTestimony));
 setInterval(() => {
     nextTestimony(currentTestimony);
     testimonyCount()
@@ -267,10 +341,22 @@ setInterval(() => {
 
 
 function setTestimony(testimonySlide) {
+
     testimonialSlide.forEach(testimonial => {
-        testimonial.style.display = 'none';
+        let body = document.querySelector('body')
+        const observeTestimonies = new ResizeObserver(entries => {
+            body = entries[0]
+            const isMobile = body.contentRect.width >= 960;
+            if (isMobile === true) {
+                testimonial.style.display = 'flex';
+            } else {
+                testimonial.style.display = 'none';
+            }
+            testimonialSlide[testimonySlide].style.display = 'block';
+        })
+        observeTestimonies.observe(body)
     })
-    testimonialSlide[testimonySlide].style.display = 'block';
+
 
 }
 
